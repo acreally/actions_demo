bfd42fb002758135a6ec78fc1982ee6b939cf939
acreally <creallya@gmail.com>
2019-11-04 15:07:47 -0500

    Addressing lint error.

--- src/date_util.py
+++ src/date_util.py
@@ -10,6 +10,9 @@ FORMAT_STRING = '%A, %B %d, %Y, %I:%M:%S %p'
 
 
 def convert_format(date_string, current_format_string):
+    '''
+        Convert a date string from its format to a standard format.
+    '''
     if not date_string or not current_format_string:
         return None
 
